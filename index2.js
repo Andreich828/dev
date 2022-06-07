8df9a4bbf5d99ae8b9bff3267e3e231be4ad5d2d
Alexey Sklyar <70955904+Andreich828@users.noreply.github.com>
2022-06-07 13:40:01 +0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,12 @@
+let user = {
+    name: "Alexey";
+    age: 27;
+    user.isMale = true;
+    myCar = "Volosatiy motoroller"
+    myCash = undefined;
+    mySalary = null;
+    myTruesalary = 37400;
+    myProfession = "Engineer"
+
+
+}
