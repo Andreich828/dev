392517d765b502db1fd862283c87fcd641050022
unknown <saionara5464@gmail.com>
2022-06-23 20:41:22 +1100

    Перерал варианты ипотеки

--- Cicle.js
+++ Cicle.js
@@ -1,86 +1,22 @@
-//declaration
-
-// function sum(a,b) {
-//     return a + b;
-// };
-// const result = sum(6,4); 
-// console.log(result);
-
-// function plus(a,b) {
-//     return a * b;
-// };
-// const umnozhenie = plus(7,10);
-// console.log(umnozhenie);
-
-// function minus(a,b,c) {
-//     return a + b - c;
-// };
-// const vichitanie = minus(9,10,8);
-// console.log(vichitanie);
-
-// function delenie(a,c) {
-//     return c / a;
-// };
-// const podelil = delenie(90,9);
-// console.log(podelil)
-
-// function word(word1, word2) {
-//     return word1 + word2;
-// };
-// const words = word('Privet',' Andrei')
-// console.log(words)
+const basicPercent = 15; //базовый процент
+const borrowers = ["семейная ипотека","льготная ипотека","обычная","ипотека для IT-специалистов"]; //набор ипотек
+const discounts = [8, 13, 0, 5.5] //скидки
 
 
 
-// //expression
-// let word = (a,b) => a + b;
-// alert( word ("Mladshiy"," Leitenant"));
-
-const a = 5;
-const b = 4;
-const result = a > b
-console.log(result) // true
-
-const pineapple = 'ананас';
-const apple = 'яблоко';
-const result1 = pineapple > apple;
-console.log(result1) // false
-
-const c = '2';
-const d = '12';
-const result2 = c > d;
-console.log(result2); // true
-
-const und = undefined;
-const nu = null;
-const result3 = und == nu;
-console.log(result3); // true
-
-const defined = undefined;
-const ll = null;
-const result4 = defined === ll;
-console.log(result4); // false
-
-const zero = null;
-const hz = "\n0\n";
-const result5 = zero == hz;
-console.log(result5); // false
-
-const nol = null;
-const na = +"\n0\n";
-const result6 = nol === na;
-console.log(result6); // false
-
-
-const day = "1930";
-if (day == "morning") {
-    console.log('Ешь бутерброд,и хуярь на любимую работу')
-} else if (day == "noon") {
-    console.log("Пора обедать! Кушай борщ и возвращайся на любимую работу")
-} else if (day == "evening") {
-    console.log("Вернулся с любимой работы,мой жопу, иди ужинать!")
-} else if (day == "night") {
-    console.log("Включи сериал, и засыпай! Завтра на любимую работу!")
-} else {
-    console.log("Нечем заняться? Иди поешь!")
+for(let i=0; i< borrowers.length; i+=1){
+    if (i==0){
+        totalPercent = basicPercent - discounts[0];
+        console.log(borrowers[i] + ' : ' + totalPercent + ' процентов годовых');
+    } else if (i==1){
+        totalPercent = basicPercent - discounts[1];
+        console.log(borrowers[i] + ' : ' +totalPercent + ' процентов годовых');
+    } else if (i==2) {
+        totalPercent = basicPercent - discounts[2];
+        console.log(borrowers[i] + ' : ' + totalPercent + ' процентов годовых');
+    } else if (i==3) {
+        totalPercent = basicPercent - discounts[3];
+        console.log(borrowers[i] + ' : ' +totalPercent + ' процентов годовых');
+    }
 }
+    
